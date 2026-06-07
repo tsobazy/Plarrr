ca20a30c203f7311e6598788f15426eb44cbf235
tsobazy <tsobazy@gmail.com>
2026-06-06 21:49:40 -0700

    fix load_data to use parquet from file/

--- R/load_data.R
+++ R/load_data.R
@@ -1,18 +1,18 @@
 #' Load plastic waste data
 #'
 #' Loads the plastic waste dataset directly
-#' from TidyTuesday
+#' from the package
 #'
 #' @return A data frame with columns that include country, year, parent_company,
 #'   plastic type totals (hdpe, ldpe, pet, pp, ps, pvc), grand_total,
 #'   num_events, volunteer, and other country level metrics.
 #' @export
-#' @importFrom readr read_csv
+#' @importFrom arrow read_parquet
 #' @examples
 #' df <- load_data()
 #' head(df)
 load_data <- function() {
-  readr::read_csv(
-    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-26/plastics.csv'
-  )
+  path <- system.file("population_plastics.parquet", package = "Plarrr")
+  arrow::read_parquet(path)
 }
+
